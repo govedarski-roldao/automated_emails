09b777f8c2a5953f2f370b298b1129d3198a3acb
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2025-05-31 23:28:34 +0300

    email created

---
+++ emailsss.py
@@ -0,0 +1,7 @@
+import yagmail
+
+email = yagmail.SMTP("roldao1990@gmail.com", "lmyz saok rpwq lczq")
+email.send("aibdxqujhzwahv@dropmail.me",
+           "test",
+           "Hello, this is the body of the test!\n Rodolfo",
+           "design.txt")
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import requests
+import time
+from pprint import pprint
+import imaplib
+import emailsss
+
+
+
 class ExcelFile:
     def __init__(self, filepath):
         self.filepath = filepath
@@ -7,19 +15,53 @@ class ExcelFile:
 
 
 class Email:
-    def __init__(self, sender, receiver, subject, body):
-        self.sender = sender
-        self.receiver = receiver
-        self.subject = subject
-        self.body = body
+    def __init__(self):
+        pass
+        # #, sender, receiver, subject, body
+        # self.sender = sender
+        # self.receiver = receiver
+        # self.subject = subject
+        # self.body = body
 
     def send(self):
         pass
 
 
 class NewsFeed:
-    def __init__(self, data):
-        self.data = data
+    """Representing multiple news titles and links as a single string"""
+    API_KEY = "&apiKey=b7c81e35e1c942ee831f80be029826b0"
+    URL_BASE = "https://newsapi.org/v2/everything?"
+    RAW_URL_HEADLINES = "https://newsapi.org/v2/top-headlines?country=us"
+    RAW_URL_SOURCE = "https://newsapi.org/v2/top-headlines?sources=bbc-news"
+    RAW_URL_TOPIC = ("https://newsapi.org/v2/everything?"
+                     "q=coimbra&"
+                     "from=2025-05-28&"
+                     "language=pt&"
+                     "to=2025-05-29")
+
+    def __init__(self, interest, from_date, to_date, language):
+        self.email_body = ""
+        self.interest = f"q={interest}&"
+        self.from_date = f"from={from_date}&"
+        self.to_date = f"to={to_date}"
+        self.language = f"language={language}&"
+
+    def prepare_body(self, dictionary_of_news):
+        for line in dictionary_of_news:
+            if line["title"] and line["url"]:
+                self.email_body += line["title"] + "\n" + line["url"] + "\n\n"
+        return self.email_body
 
     def get(self):
-        pass
+        url = self.URL_BASE + self.interest + self.from_date + self.language + self.to_date + self.API_KEY
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(f"Erro na requisição: {response.status_code} - {response.text}")
+            return ""
+        content = response.json()["articles"]
+        body = self.prepare_body(content)
+        return body
+
+
+news = NewsFeed("coimbra","2025-05-28","2025-05-30","pt")
+print(news.get())
