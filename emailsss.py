a3cb80e54996d7e3ce09ce6b4e866f04c40dea8a
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2025-06-01 00:24:56 +0300

    excel reader

--- emailsss.py
+++ emailsss.py
@@ -1,7 +1,14 @@
+##https://dropmail.me/pt/
 import yagmail
+import pandas
 
-email = yagmail.SMTP("roldao1990@gmail.com", "lmyz saok rpwq lczq")
-email.send("aibdxqujhzwahv@dropmail.me",
-           "test",
-           "Hello, this is the body of the test!\n Rodolfo",
-           "design.txt")
+
+df = pandas.read_excel("people.xlsx")
+
+for index, row in df.iterrows():
+    print(row["email"])
+    email = yagmail.SMTP("roldao1990@gmail.com", "lmyz saok rpwq lczq")
+    email.send(row["email"],
+               f"Your {row["interest"]} news for today",
+               f"Hello {row["name"]}\n check this out!\n Rodolfo",
+               "design.txt")
